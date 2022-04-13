b87c1d8507a67049482e88ae2363133053f2ae23
Fan Jing <jing1999fan@outlook.com>
2022-04-13 00:49:20 -0700

    Add project files.

---
+++ InAndOut/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> txt = new List<string>();
+            StreamReader source_read = new StreamReader("C://Users/admin/Desktop/text.txt");
+            while (source_read.ReadLine() != null)
+            {
+                txt.Add(source_read.ReadLine());
+            }
+            source_read.Close();
+
+            StreamWriter source_write = new StreamWriter("C://Users/admin/Desktop/test.txt");
+            for (int i=0; i<txt.Count; i++)
+            {
+                source_write.WriteLine(txt[i]);
+            }
+            source_write.Flush();
+            source_write.Close();
+        }
+    }
+}
